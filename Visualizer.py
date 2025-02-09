05263e31fc8377ac8afb8c3a056878d25abdc8da
filbert14 <filbertfebrianto14@gmail.com>
2025-02-09 13:19:11 +0100

    Add method to save images

--- Visualizer.py
+++ Visualizer.py
@@ -1,5 +1,14 @@
-def main():
-    pass
+import os
 
-if __name__ == "__main__":
-    main()
+import cv2
+from tqdm import tqdm
+
+class Visualizer:
+    def __init__(self):
+        pass
+
+    def save2D(self, images, save_dir):
+        os.mkdir(save_dir)
+
+        for i, image in enumerate(tqdm(images, desc="Saving images")):
+            cv2.imwrite(os.path.join(save_dir, f"{i:06}.png"), image)
