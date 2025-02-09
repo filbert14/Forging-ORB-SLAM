2bd007aa7dfc8f9c1dcffbda34c9d4264b17a890
filbert14 <filbertfebrianto14@gmail.com>
2025-02-09 19:07:03 +0100

    Create a deep copy with Frame

--- Frame.py
+++ Frame.py
@@ -1,13 +1,21 @@
+import cv2
+
 class Frame:
     def __init__(self, image, keypoints, descriptors):
         # Original image
-        self.image       = image
+        self.image       = image.copy()
 
         # List of keypoints
-        self.keypoints   = keypoints
+        self.keypoints   = [cv2.KeyPoint(x        = keypoint.pt[0],
+                                         y        = keypoint.pt[1],
+                                         size     = keypoint.size,
+                                         angle    = keypoint.angle,
+                                         response = keypoint.response,
+                                         octave   = keypoint.octave,
+                                         class_id = keypoint.class_id) for keypoint in keypoints]
 
         # List of descriptors
-        self.descriptors = descriptors
+        self.descriptors = descriptors.copy()
 
         # Number of keypoints
         self.N           = len(keypoints)
