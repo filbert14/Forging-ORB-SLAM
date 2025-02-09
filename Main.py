3be283afb74b961dcd09474e316a75ac7a5841fb
filbert14 <filbertfebrianto14@gmail.com>
2025-02-09 17:37:36 +0100

    Add ORB features

--- Main.py
+++ Main.py
@@ -1,9 +1,14 @@
+import yaml
+
 from EDSLoader import EDSLoader
 from Tracking  import Tracking
 
 def main():
+    with open("Settings.yaml", "r") as file:
+        settings = yaml.safe_load(file)
+
     edsloader = EDSLoader("EDS", "09_ziggy_flying_pieces")
-    tracking  = Tracking(edsloader.images)
+    tracking  = Tracking(edsloader.images, settings)
 
 if __name__ == "__main__":
     main()
--- Tracking.py
+++ Tracking.py
@@ -8,11 +8,28 @@ class State(Enum):
     INITIALIZING    = 2
 
 class Tracking:
-    def __init__(self, images):
+    def __init__(self, images, settings):
         # Class variables
-        self.images  = images
-        self.img_idx = 0
-        self.state   = State.NO_IMAGES_YET
+        self.images   = images
+        self.settings = settings
+
+        self.img_idx  = 0
+        self.state    = State.NO_IMAGES_YET
+
+        # ORB
+        nfeatures     = settings["ORBextractor.nFeatures"]
+        scaleFactor   = settings["ORBextractor.scaleFactor"]
+        nlevels       = settings["ORBextractor.nLevels"]
+        fastThreshold = settings["ORBextractor.fastTh"]
+        scoreType     = settings["ORBextractor.nScoreType"]
+
+        self.orb = cv2.ORB_create(
+            nfeatures     = nfeatures,
+            scaleFactor   = scaleFactor,
+            nlevels       = nlevels,
+            fastThreshold = fastThreshold,
+            scoreType     = scoreType,
+        )
 
     def FirstInitialization(self):
         print("Hello world!")
