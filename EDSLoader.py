30ddf469cc95375f25b7cd8aa891e723591d2002
filbert14 <filbertfebrianto14@gmail.com>
2025-02-09 11:57:28 +0100

    Add EDSLoader.py

---
+++ EDSLoader.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+import cv2
+from tqdm import tqdm
+
+class EDSLoader:
+    def __init__(self, dataset, sequence):
+        self.dataset  = dataset
+        self.sequence = sequence
+
+        images_path = os.path.join(self.dataset, self.sequence, "images")
+
+        image_files = [image_file for
+                       image_file in
+                       sorted(os.listdir(images_path)) if
+                       not image_file == "timestamps.txt"]
+
+        self.images = []
+        for image_file in tqdm(image_files, desc="Loading images"):
+            self.images.append(cv2.imread(os.path.join(images_path, image_file), cv2.IMREAD_GRAYSCALE))
+
+def main():
+    EDSLoader("EDS", "09_ziggy_flying_pieces")
+
+if __name__ == "__main__":
+    main()
