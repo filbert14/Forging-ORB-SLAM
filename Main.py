b8221d2e520a297b03738d4467de74e2976e37f1
filbert14 <filbertfebrianto14@gmail.com>
2025-02-09 17:20:56 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,9 @@
+from EDSLoader import EDSLoader
+from Tracking  import Tracking
+
 def main():
-    print("Hello world!")
+    edsloader = EDSLoader("EDS", "09_ziggy_flying_pieces")
+    tracking  = Tracking(edsloader.images)
 
 if __name__ == "__main__":
     main()
